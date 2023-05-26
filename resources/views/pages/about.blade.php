29b0cb9ba6c9db248405fbb0f6fe59b8d47222b1
itkoncept <khadher.it.koncept@gmail.com>
2023-05-26 13:23:49 +0200

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us | Helllo City</title>
-</head>
-<body>
+@extends('base')
+
+@section('content')
+
     <p>Built with &hearts; by Belgacem Khadher.</p>
     <p><a href="/">Revenir à la page d'accueil</a></p>
 
-    <footer>
-        <p>&copy; Copyright {{date("Y")}}</p>
-    </footer>
-</body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Helllo City</title>
-</head>
-<body>
+@extends('base')
+
+@section('content')
+
     <h1>Hello from Tunis</h1>
     <p>It's currently {{date("h:i A")}}.</p>
 
-    <footer>
-        <p>
-            &copy; Copyright {{date("Y")}}
-            &middot; <a href="/about-us"> About Us</a>
-        </p>
-    </footer>
-</body>
-</html>
+@endsection
